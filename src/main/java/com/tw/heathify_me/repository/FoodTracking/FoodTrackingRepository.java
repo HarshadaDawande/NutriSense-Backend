b700a8ead693aff4209f33f3051412474d63e3c7
HarshadaDawande <harshadadawande@gmail.com>
2025-06-26 16:51:19 +0530

    Harshada | Add ObjectId in repositories

--- src/main/java/com/tw/heathify_me/repository/FoodTracking/FoodTrackingRepository.java
+++ src/main/java/com/tw/heathify_me/repository/FoodTracking/FoodTrackingRepository.java
@@ -1,9 +1,10 @@
 package com.tw.heathify_me.repository.FoodTracking;
 
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FoodTrackingRepository extends MongoRepository<FoodTrackingDocument, String> {
+public interface FoodTrackingRepository extends MongoRepository<FoodTrackingDocument, ObjectId> {
 
 }
